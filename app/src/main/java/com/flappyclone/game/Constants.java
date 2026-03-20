1009042ea5186c6d0bc2732a82595514973c5e1b
Maister <138547658+smsihab@users.noreply.github.com>
2026-03-20 08:30:25 +0600

    Add Constants.java for game constants and configuration

---
+++ app/src/main/java/com/flappyclone/game/Constants.java
@@ -0,0 +1,16 @@
+package com.flappyclone.game;
+
+public class Constants {
+    // Game Constants
+    public static final int FLOOR_HEIGHT = 50;
+    public static final int GRAVITY = 9;
+    public static final int BIRD_FLAP_POWER = 15;
+    public static final int PIPE_SPEED = 5;
+    public static final int PIPE_GAP_HEIGHT = 200;
+
+    // Game Configuration
+    public static final String GAME_NAME = "Flappy Clone";
+    public static final int SCREEN_WIDTH = 800;
+    public static final int SCREEN_HEIGHT = 600;
+    public static final int MAX_SCORE = 100;
+}
